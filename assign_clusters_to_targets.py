0352f05b6d50b769dd33c84e36c8c996b3fce05d
Boris Efraty <efratyb@gmail.com>
2020-06-30 23:59:17 +0300

    added probabily distribution of color and type per cluster

--- assign_clusters_to_targets.py
+++ assign_clusters_to_targets.py
@@ -4,3 +4,13 @@ import cluster_utils
 #each target is mixture of up to 3 targets
 
 #TODO
+#load targets.csv
+
+#load clustered.csv
+
+#compute scores (KL) sklearn.metrics.mutual_info_score
+
+#solve assignment
+
+
+# add target column

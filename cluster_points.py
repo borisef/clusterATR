82931b9d80b864c68975069bd8ed98d7cba52c2b
Boris Efraty <efratyb@gmail.com>
2020-07-02 00:22:47 +0300

    assignment almost done, till matrix form

--- cluster_points.py
+++ cluster_points.py
@@ -6,9 +6,24 @@ from sklearn import cluster
 from scipy.cluster.hierarchy import fclusterdata
 from scipy import stats
 import cluster_utils
+import pickle
+
+from cluster_utils import hotEncodeColors
+from cluster_utils import hotEncodeTypes
+from cluster_utils import ConvertConfMatrix2ProbMatrix
+from cluster_utils import ProbabilityFromDistance
 
 
 
+
+#inputs
+all_results_in_csv_name = "data/all_results.csv" # data table with all results
+confmType_csv_name = "data/confmType.csv" # confusion matrix type classifier
+confmColor_csv_name = "data/confmColor.csv" # confusion matrix color classifier
+
+
+
+#parameters
 LOCATE_SIGMA = 5 # mean error of igun
 WEIGHT_LOC = 0.5 # distance "expert" weight
 WEIGHT_COLOR = 0.2 # color "expert" weight
@@ -20,56 +35,8 @@ TYPES_CREDIT = 0.1 # our belief in possibility of most crazy type combination fo
 
 FCLUSTER_THRESHOLD = 0.7 # threshold on fclusterdata , between [0, 1] , small ==> many clusters , large ==> few clusters
 
-all_results_in_csv_name = "data/all_results.csv" # data table with all results
-confmType_csv_name = "data/confmType.csv" # confusion matrix type classifier
-confmColor_csv_name = "data/confmColor.csv" # confusion matrix color classifier
-
-hotEncodeColors = { 0: 'black', 1: 'blue', 2: 'gray',  3: 'green',4: 'red', 5: 'white'}
-hotEncodeTypes = { 1: 'UNKNOWN_SUB_CLASS', 2: 'PRIVATE', 3: 'COMMERCIAL',  4: 'PICKUP',5: 'TRUCK', 6: 'BUS', 7: 'VAN', 8: 'TRACKTOR'}
-
-
 priorsColor = np.ones(shape=(6,1))/6 # prior believes color
-priorsClass = np.ones(shape=(8,1))/8 # prior believes types
-
-
-
-def ProbabilityFromDistance(d,sigma,pow=1):
-    #distance in meters
-    #sigma = standard deviation
-    #pow - amplify
-    pr = np.exp(-np.power(d/sigma,2))
-    pr = np.power(pr,pow)
-    return pr
-
-def ConvertConfMatrix2ProbMatrix(M, priors = None, credit = 0):
-    # M is is NxN, priors - Nx1 (1/N default)
-
-    N = M.shape[0]
-    if(priors is None):
-        priors = np.ones(shape=(N,1))/N
-
-    M_probs = np.zeros_like(M)
-    M_probs_same_class = np.zeros_like(M)
-    for i in range(N):
-        for j in range(N):
-            for i1 in range(N):
-                j1 = i1 # same class
-                M_probs_same_class[i,j] += priors[i1]*M[i1,i]* priors[i1]*M[i1,j]
-    for i in range(N):
-        for j in range(N):
-            for i1 in range(N):
-                for j1 in range(N):
-                    M_probs[i,j] += priors[i1]*M[i1,i]* priors[j1]*M[j1,j]
-
-    M_probs_cond = M_probs_same_class/(M_probs+ 0.00001)
-    M_probs_cond = M_probs_cond*(1-credit) + credit
-
-
-    return M_probs_cond
-
-def ProbabilityFromProbMatrix(PM, c1,c2):
-    return PM[c1-1,c2-1]
-
+priorsClass = np.ones(shape=(8,1))/8 # prior believes types/classes
 
 #load data from csv
 df = pd.read_csv(all_results_in_csv_name)
@@ -94,25 +61,23 @@ def similarity(x,y):
         #print(dis)
         return dis
 
-    d = np.power(np.power(x[1] - y[1],2) + np.power(x[2] - y[2],2),0.5)
+    d = np.power(np.power(x[1] - y[1],2) + np.power(x[2] - y[2],2),0.5) # euclidean distance
     prDist = ProbabilityFromDistance(d,LOCATE_SIGMA)
     prCol = probColors[int(x[4]-1), int(y[4]-1)]
     prClass = probClasses[int(x[3]-1), int(y[3]-1)]
 
-
+    #weighted average
     prTotal = WEIGHT_LOC*prDist + WEIGHT_COLOR*prCol + WEIGHT_CLASS*prClass
     #print((1-prTotal))
     return (1.0 - prTotal)
 
 
-
 fclust1 = fclusterdata(X = df, t = FCLUSTER_THRESHOLD , metric=similarity, criterion='distance', method='complete')
 numClust = len(np.unique(fclust1))
-print(numClust)
+print("Num clusters" + str(numClust))
 
 df['label'] = fclust1
-#aa = cluster.DBSCAN(eps=0.3, min_samples=1, metric= similarity).fit_predict(df)
-#aa2 = cluster.OPTICS(min_samples=1,metric = similarity, eps = 0.3).fit_predict(df)
+#aa = cluster.DBSCAN(eps=0.3, min_samples=1, metric= similarity).fit_predict(df) # another clustering to consider
 
 
 df = cluster_utils.AddText(df)
@@ -127,20 +92,24 @@ df_final = (df.groupby('label').agg({
     'class': [lambda x: stats.mode(x)[0], lambda x: list(x)],
     'color': [lambda x: stats.mode(x)[0], lambda x: list(x)] }) ).reset_index()
 
-# probability distribution functions (color and type)
-df_final= cluster_utils.addFrequencies(df_final,df,'color')
-df_final= cluster_utils.addFrequencies(df_final,df,'class')
-
-
 df_final.columns = ["_".join(x) for x in df_final.columns.ravel()]
+
 df_final.rename(columns={'class_<lambda_0>':'class',
                          'color_<lambda_0>':'color',
                          'class_<lambda_1>':'all_class',
                          'color_<lambda_1>':'all_color',
                          'x_median':"x",
-                         'y_median':'y'
+                         'y_median':'y',
+                         "label_":'label'
                          },
                  inplace=True)
+
+
+# probability distribution functions (color and type)
+df_final= cluster_utils.addFrequencies(df_final,df,'color')
+df_final= cluster_utils.addFrequencies(df_final,df,'class')
+
+
 
 df_final = cluster_utils.AddTextDff(df_final)
 
@@ -154,10 +123,8 @@ cluster_utils.plot_scatter(df,'class','results/types.png')
 cluster_utils.plot_scatter(df,'label','results/clusters.png', False)
 cluster_utils.plot_scatter(df,'label','results/clusters_with_text.png', True)
 
-#draw df_final with text etc
+#draw  with text etc
 cluster_utils.plot_scatter(df_final,'class','results/final_with_text.png', True)
-
-
 #plot nicer
 cluster_utils.plot_scatter_final(df_final_no_false,'results/final_with_text_no_false.png', True)
 
@@ -165,6 +132,11 @@ cluster_utils.plot_scatter_final(df_final_no_false,'results/final_with_text_no_f
 df_final.to_csv("results/clustered.csv")
 df_final_no_false.to_csv("results/clustered_without_false.csv")
 df.to_csv("results/original_clustered.csv")
+
+#save pickles
+df_final.to_pickle("results/clustered.pckl")
+df_final_no_false.to_pickle("results/clustered_without_false.pckl")
+df.to_pickle("results/original_clustered.pckl")
 
 
 print("OK")
--- cluster_utils.py
+++ cluster_utils.py
@@ -2,35 +2,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import collections
-import scipy.stats
+from scipy import stats
 import sklearn.metrics
+from scipy.spatial import distance
 
 hotEncodeColors = { 0: 'black', 1: 'blue', 2: 'gray',  3: 'green',4: 'red', 5: 'white'}
 hotEncodeTypes = { 1: 'UNKNOWN_SUB_CLASS', 2: 'PRIVATE', 3: 'COMMERCIAL',  4: 'PICKUP',5: 'TRUCK', 6: 'BUS', 7: 'VAN', 8: 'TRACKTOR'}
+smallestColor = 0
+smallestType = 1
 
+def ConvertConfMatrix2ProbMatrix(M, priors = None, credit = 0):
+    # M is is NxN
+    # priors - Nx1 (1/N default)
+    #return pseudo-distance as (1-prob)
 
+    N = M.shape[0]
+    if(priors is None):
+        priors = np.ones(shape=(N,1))/N
 
-def jensen_shannon_distance(p, q):
-    """
-    method to compute the Jenson-Shannon Distance
-    between two probability distributions
-    """
+    M_probs = np.zeros_like(M)
+    M_probs_same_class = np.zeros_like(M)
+    for i in range(N):
+        for j in range(N):
+            for i1 in range(N):
+                j1 = i1 # same class
+                M_probs_same_class[i,j] += priors[i1]*M[i1,i]* priors[i1]*M[i1,j]
+    for i in range(N):
+        for j in range(N):
+            for i1 in range(N):
+                for j1 in range(N):
+                    M_probs[i,j] += priors[i1]*M[i1,i]* priors[j1]*M[j1,j]
 
-    # convert the vectors into numpy arrays in case that they aren't
-    p = np.array(p)
-    q = np.array(q)
+    M_probs_cond = M_probs_same_class/(M_probs+ 0.00001)
+    M_probs_cond = M_probs_cond*(1-credit) + credit
 
-    # calculate m
-    m = (p + q) / 2
+    return M_probs_cond
 
-    # compute Jensen Shannon Divergence
-    divergence = (scipy.stats.entropy(p, m) + scipy.stats.entropy(q, m)) / 2
-
-    # compute the Jensen Shannon Distance
-    distance = np.sqrt(divergence)
-
-    return distance
-
+def ProbabilityFromDistance(d,sigma,pow=1):
+    #distance in meters
+    #sigma = standard deviation
+    #pow - amplify
+    pr = np.exp(-np.power(d/sigma,2))
+    pr = np.power(pr,pow)
+    return pr
 
 def GetFrequencies(arr,column_name):
     elements_count = collections.Counter(arr)
@@ -50,7 +64,7 @@ def GetFrequencies(arr,column_name):
 
 
 def addFrequencies(aggDF, refDF, column_name):
-    new_col_name = column_name + "_freq"
+    new_col_name = "freq_" + column_name
     mylist = []
     for i in range(aggDF.shape[0]):
         lbl = aggDF["label"][i]
@@ -135,3 +149,53 @@ def plot_scatter_final(df, save2im, withText = True):
     plt.savefig(save2im)
     plt.title("grp_by," + save2im[:-4] )
     plt.show()
+
+
+def AggregateTargets(df_targets):
+    agg_df_targets = (df_targets.groupby('target').agg({
+    'score': ['sum'],
+    'subclass': [ lambda x: list(x)],
+    'color': [lambda x: list(x)] }) ).reset_index()
+    agg_df_targets.columns = ["_".join(x) for x in agg_df_targets.columns.ravel()]
+    agg_df_targets.columns = ["target", "total_score", "all_classes", "all_colors"]
+    numTargets = agg_df_targets.shape[0]
+    numCols = len(hotEncodeColors)
+    numTypes = len(hotEncodeTypes)
+    listFreqColor = []
+    listFreqType = []
+
+    for target in agg_df_targets['target']:
+        print(target)
+        freqColor = np.zeros(numCols, dtype = float)
+        freqType = np.zeros(numTypes, dtype=float)
+        df = df_targets[df_targets["target"] == target]
+        clrs = np.array(df.color, dtype=int)
+        typs = np.array(df.subclass, dtype=int)
+        sc = np.array(df.score)
+        for i,co in enumerate(clrs):
+            freqColor[co - smallestColor] += sc[i]
+            freqType[typs[i] - smallestType] += sc[i]
+        listFreqColor.append(freqColor)
+        listFreqType.append(freqType)
+
+    agg_df_targets["freq_color"] = listFreqColor
+    agg_df_targets["freq_class"] = listFreqType
+
+    return(agg_df_targets)
+
+
+
+def ComputeDistancesTargets2Clusters(df_T, df_C, wt, bonus_type, bonus_color):
+    NC = df_C.shape[0] # num clusters
+    NT = df_T.shape[0] # num targets
+    D = np.zeros((NC,NT), dtype=float)
+    DTypes = np.zeros_like(D, dtype=float)
+    DCcolors = np.zeros_like(D, dtype=float)
+    for tar in range(NT):
+        for clus in range(NC):
+            DCcolors[clus,tar]= distance.cosine(df_T["freq_color"][tar] + bonus_color, df_C["freq_color"][clus] + bonus_color)
+            DTypes[clus,tar] = distance.cosine(df_T["freq_class"][tar] + bonus_type, df_C["freq_class"][clus] + bonus_type)
+
+    D = DTypes * wt + DCcolors * (1.0 - wt)
+    return D
+

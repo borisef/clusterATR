355b31c564d5132de0c0d2bb771e770cb2abc441
Boris Efraty <efratyb@gmail.com>
2020-07-05 22:34:24 +0300

    did changes for recogs.txt

--- cluster_points.py
+++ cluster_points.py
@@ -11,7 +11,7 @@ import pickle
 from cluster_utils import hotEncodeColors
 from cluster_utils import hotEncodeTypes
 from cluster_utils import ConvertConfMatrix2ProbMatrix
-from cluster_utils import ProbabilityFromDistance
+from cluster_utils import ProbabilityFromDistance, ReadCSV_or_TXT
 
 
 
@@ -21,9 +21,16 @@ TYPES_CREDIT = 0.1 # our belief in possibility of most crazy type combination fo
 FCLUSTER_THRESHOLD = 0.8 # threshold on fclusterdata , between [0, 1] , small ==> many clusters , large ==> few clusters
 
 #inputs
-all_results_in_csv_name = "data/all_results.csv" # data table with all results
+#all_results_in_csv_name = "data/all_results.csv" # data table with all results
+#input_data_type = "type1" # like all_results.csv
+
+all_results_in_csv_name = "data/recogs.txt" # data table with all results
+input_data_type = "type2" # like all_results.csv
+
+
 confmType_csv_name = "data/confmType.csv" # confusion matrix type classifier
 confmColor_csv_name = "data/confmColor.csv" # confusion matrix color classifier
+
 
 
 
@@ -32,18 +39,19 @@ LOCATE_SIGMA = 5 # mean error of igun
 WEIGHT_LOC = 0.5 # distance "expert" weight
 WEIGHT_COLOR = 0.2 # color "expert" weight
 WEIGHT_CLASS = 1 - WEIGHT_LOC - WEIGHT_COLOR
-FRAME_COUNT_THRESHOLD = 1  # min frames per target
+FRAME_COUNT_THRESHOLD = 3  # min frames per target
 
 COLOR_CREDIT = 0.1 # our belief in possibility of most crazy color combination for same target
 TYPES_CREDIT = 0.1 # our belief in possibility of most crazy type combination for same target
 
 FCLUSTER_THRESHOLD = 0.7 # threshold on fclusterdata , between [0, 1] , small ==> many clusters , large ==> few clusters
 
-priorsColor = np.ones(shape=(6,1))/6 # prior believes color
+priorsColor = np.ones(shape=(7,1))/7 # prior believes color
 priorsClass = np.ones(shape=(8,1))/8 # prior believes types/classes
 
 #load data from csv
-df = pd.read_csv(all_results_in_csv_name)
+df = ReadCSV_or_TXT(all_results_in_csv_name, input_data_type)
+
 
 #load confusion matrices
 conf1 = np.genfromtxt(confmType_csv_name, delimiter=',')
@@ -54,8 +62,8 @@ probColors = ConvertConfMatrix2ProbMatrix(conf2,priorsColor, COLOR_CREDIT)
 probClasses = ConvertConfMatrix2ProbMatrix(conf1,priorsClass, TYPES_CREDIT)
 
 
-def similarity(x,y):
-    #x, y = Frame,x,y,class,color,ObjID
+def similarityType1(x,y):
+    #x, y = frame_idx,X,Y,cls,clr,target_id
     if(x[0]==y[0]):#same frame
         dis = 1.0
         #print(dis)
@@ -64,6 +72,7 @@ def similarity(x,y):
         dis = 0.0
         #print(dis)
         return dis
+
 
     d = np.power(np.power(x[1] - y[1],2) + np.power(x[2] - y[2],2),0.5) # euclidean distance
     prDist = ProbabilityFromDistance(d,LOCATE_SIGMA)
@@ -75,13 +84,43 @@ def similarity(x,y):
     #print((1-prTotal))
     return (1.0 - prTotal)
 
+def similarityType2(x,y):
+    #x, y = frame_idx target_id ts daytime X Y Z lat lon x y w h clr clr_score cls cls_score
+    if(x[0]==y[0]):#same frame
+        dis = 1.0
+        #print(dis)
+        return dis
+    if(x[1] == y[1] and x[1]>0):#same object
+        dis = 0.0
+        #print(dis)
+        return dis
 
-fclust1 = fclusterdata(X = df, t = FCLUSTER_THRESHOLD , metric=similarity, criterion='distance', method='complete')
+    d = np.power(np.power(x[4] - y[4],2) + np.power(x[5] - y[5],2),0.5) # euclidean distance
+    prDist = ProbabilityFromDistance(d,LOCATE_SIGMA)
+    prCol = probColors[int(x[13]-1), int(y[13]-1)] # clr
+    prClass = probClasses[int(x[15]-1), int(y[15]-1)] # cls
+
+    #weighted average
+    prTotal = WEIGHT_LOC*prDist + WEIGHT_COLOR*prCol + WEIGHT_CLASS*prClass
+    #print((1-prTotal))
+    return (1.0 - prTotal)
+
+
+
+if(input_data_type is "type1"):
+    similarity = similarityType1
+    df4clust = df
+else:
+    similarity = similarityType1 # TODO define your own
+    df4clust = df[['frame_idx', "X", "Y", "cls", "clr", "target_id"]]
+
+
+fclust1 = fclusterdata(X = df4clust, t = FCLUSTER_THRESHOLD , metric=similarity, criterion='distance', method='complete')
 numClust = len(np.unique(fclust1))
 print("Num clusters" + str(numClust))
 
 df['label'] = fclust1
-#aa = cluster.DBSCAN(eps=0.3, min_samples=1, metric= similarity).fit_predict(df) # another clustering to consider
+#aa = cluster.DBSCAN(eps=0.3, min_samples=1, metric= similarityType1).fit_predict(df) # another clustering to consider
 
 
 df = cluster_utils.AddText(df)
@@ -89,46 +128,46 @@ df = cluster_utils.AddText(df)
 
 #major voting per cluster
 df_final = (df.groupby('label').agg({
-    'x': 'median',
-    'y': 'median',
-    'Frame': ['min', 'max', 'count'],
-    'ObjID': 'count',
-    'class': [lambda x: stats.mode(x)[0], lambda x: list(x)],
-    'color': [lambda x: stats.mode(x)[0], lambda x: list(x)] }) ).reset_index()
+    'X': 'median',
+    'Y': 'median',
+    'frame_idx': ['min', 'max', 'count'],
+    'target_id': 'count',
+    'cls': [lambda x: stats.mode(x)[0], lambda x: list(x)],
+    'clr': [lambda x: stats.mode(x)[0], lambda x: list(x)] }) ).reset_index()
 
 df_final.columns = ["_".join(x) for x in df_final.columns.ravel()]
 
-df_final.rename(columns={'class_<lambda_0>':'class',
-                         'color_<lambda_0>':'color',
-                         'class_<lambda_1>':'all_class',
-                         'color_<lambda_1>':'all_color',
-                         'x_median':"x",
-                         'y_median':'y',
+df_final.rename(columns={'cls_<lambda_0>':'cls',
+                         'clr_<lambda_0>':'clr',
+                         'cls_<lambda_1>':'all_cls',
+                         'clr_<lambda_1>':'all_clr',
+                         'X_median':"X",
+                         'Y_median':'Y',
                          "label_":'label'
                          },
                  inplace=True)
 
 
 # probability distribution functions (color and type)
-df_final= cluster_utils.addFrequencies(df_final,df,'color')
-df_final= cluster_utils.addFrequencies(df_final,df,'class')
+df_final= cluster_utils.addFrequencies(df_final,df,'clr')
+df_final= cluster_utils.addFrequencies(df_final,df,'cls')
 
 
 
 df_final = cluster_utils.AddTextDff(df_final)
 
 #remove outliers
-df_final_no_false = df_final[df_final['Frame_count'] >= FRAME_COUNT_THRESHOLD]
+df_final_no_false = df_final[df_final['frame_idx_count'] >= FRAME_COUNT_THRESHOLD]
 
 
 #draw results
-cluster_utils.plot_scatter(df,'color','results/colors.png')
-cluster_utils.plot_scatter(df,'class','results/types.png')
+cluster_utils.plot_scatter(df,'clr','results/colors.png')
+cluster_utils.plot_scatter(df,'cls','results/types.png')
 cluster_utils.plot_scatter(df,'label','results/clusters.png', False)
 cluster_utils.plot_scatter(df,'label','results/clusters_with_text.png', True)
 
 #draw  with text etc
-cluster_utils.plot_scatter(df_final,'class','results/final_with_text.png', True)
+cluster_utils.plot_scatter(df_final,'cls','results/final_with_text.png', True)
 #plot nicer
 cluster_utils.plot_scatter_final(df_final_no_false,'results/final_with_text_no_false.png', True)
 
--- cluster_utils.py
+++ cluster_utils.py
@@ -6,7 +6,8 @@ from scipy import stats
 import sklearn.metrics
 from scipy.spatial import distance
 
-hotEncodeColors = { 0: 'black', 1: 'blue', 2: 'gray',  3: 'green',4: 'red', 5: 'white'}
+#hotEncodeColors = { 0: 'black', 1: 'blue', 2: 'gray',  3: 'green',4: 'red', 5: 'white', 6: 'brown'}
+hotEncodeColors = { 1: 'black', 2: 'blue', 3: 'gray',  4: 'green',5: 'red', 6: 'white', 7: 'brown'}
 hotEncodeTypes = { 1: 'UNKNOWN_SUB_CLASS', 2: 'PRIVATE', 3: 'COMMERCIAL',  4: 'PICKUP',5: 'TRUCK', 6: 'BUS', 7: 'VAN', 8: 'TRACKTOR'}
 smallestColor = 0
 smallestType = 1
@@ -21,19 +22,19 @@ def ConvertConfMatrix2ProbMatrix(M, priors = None, credit = 0):
         priors = np.ones(shape=(N,1))/N
 
     M_probs = np.zeros_like(M)
-    M_probs_same_class = np.zeros_like(M)
+    M_probs_same_cls = np.zeros_like(M)
     for i in range(N):
         for j in range(N):
             for i1 in range(N):
-                j1 = i1 # same class
-                M_probs_same_class[i,j] += priors[i1]*M[i1,i]* priors[i1]*M[i1,j]
+                j1 = i1 # same cls
+                M_probs_same_cls[i,j] += priors[i1]*M[i1,i]* priors[i1]*M[i1,j]
     for i in range(N):
         for j in range(N):
             for i1 in range(N):
                 for j1 in range(N):
                     M_probs[i,j] += priors[i1]*M[i1,i]* priors[j1]*M[j1,j]
 
-    M_probs_cond = M_probs_same_class/(M_probs+ 0.00001)
+    M_probs_cond = M_probs_same_cls/(M_probs+ 0.00001)
     M_probs_cond = M_probs_cond*(1-credit) + credit
 
     return M_probs_cond
@@ -48,12 +49,13 @@ def ProbabilityFromDistance(d,sigma,pow=1):
 
 def GetFrequencies(arr,column_name):
     elements_count = collections.Counter(arr)
-    shift = 0
-    if(column_name == "color"):
+
+    if(column_name == "clr"):
         refDict = hotEncodeColors
+        shift = 1 # if color starts from 1
     else:
         refDict = hotEncodeTypes
-        shift = 1
+        shift = 1 # if cls starts from 1
 
     outFreq = np.zeros(shape = len(refDict), dtype= float)
     for key, value in elements_count.items():
@@ -76,22 +78,22 @@ def addFrequencies(aggDF, refDF, column_name):
 
 
 def AddText(df):
-    df["text"] = "(F:" + df["Frame"].astype("str") + "," + df["color"].map(hotEncodeColors).astype("str") + "," + df[
-        "class"].map(hotEncodeTypes).astype("str") + ",id:"+ df[
-        "ObjID"].astype("str") +  ", L:" + df["label"].astype("str") +")"
+    df["text"] = "(F:" + df["frame_idx"].astype("str") + "," + df["clr"].map(hotEncodeColors).astype("str") + "," + df[
+        "cls"].map(hotEncodeTypes).astype("str") + ",id:"+ df[
+        "target_id"].astype("str") +  ", L:" + df["label"].astype("str") +")"
     return df
 
 def AddTextDff(dff):
-    dff["text"] = "(NumFrms:" + dff["Frame_count"].astype("str") + ", " + dff["color"].map(hotEncodeColors).astype("str") + \
-                  ", " + dff["class"].map(hotEncodeTypes).astype("str") +")"
+    dff["text"] = "(NumFrms:" + dff["frame_idx_count"].astype("str") + ", " + dff["clr"].map(hotEncodeColors).astype("str") + \
+                  ", " + dff["cls"].map(hotEncodeTypes).astype("str") +")"
     return dff
 
 def plot_scatter(df, grp_by, save2im, withText = False):
 
     groups = df.groupby(grp_by)
-    if(grp_by == "color"):
+    if(grp_by == "clr"):
         hotEncode = hotEncodeColors
-    if (grp_by == "class"):
+    if (grp_by == "cls"):
         hotEncode = hotEncodeTypes
     if (grp_by == "label"):
         hotEncode = np.unique(np.array(df['label']))
@@ -103,18 +105,18 @@ def plot_scatter(df, grp_by, save2im, withText = False):
     fig, ax = plt.subplots()
     ax.margins(0.05)  # Optional, just adds 5% padding to the autoscaling
     for name, group in groups:
-        if (grp_by == "color"):
-            ax.plot(group.x, group.y, marker='o', linestyle='', ms=5, label=hotEncode[name], color = hotEncode[name], markeredgecolor="black")
+        if (grp_by == "clr"):
+            ax.plot(group.X, group.Y, marker='o', linestyle='', ms=5, label=hotEncode[name], color = hotEncode[name], markeredgecolor="black")
         else:
-            ax.plot(group.x, group.y, marker='o', linestyle='', ms=5, label=hotEncode[name])
+            ax.plot(group.X, group.Y, marker='o', linestyle='', ms=5, label=hotEncode[name])
     if("objID" in df.columns):
-        groups1=df.groupby("ObjID")
+        groups1=df.groupby("target_id")
         for name, group1 in groups1:
             if(name > 0):
-                ax.plot(group1.x, group1.y, marker='', linestyle='--', ms=1)
+                ax.plot(group1.X, group1.Y, marker='', linestyle='--', ms=1)
 
-    XX = np.array(df["x"])
-    YY = np.array(df["y"])
+    XX = np.array(df["X"])
+    YY = np.array(df["Y"])
 
     if(withText):
         TT = np.array(df["text"])
@@ -134,10 +136,10 @@ def plot_scatter_final(df, save2im, withText = True):
     ax.margins(0.05)  # Optional, just adds 5% padding to the autoscaling
 
 
-    XX = np.array(df["x"])
-    YY = np.array(df["y"])
-    CC = np.array(df["color"].map(hotEncodeColors))
-    SS = np.array(df["Frame_count"])
+    XX = np.array(df["X"])
+    YY = np.array(df["Y"])
+    CC = np.array(df["clr"].map(hotEncodeColors))
+    SS = np.array(df["frame_idx_count"])
     SS = ((SS - SS.min() + 1.0)/ (SS.max() - SS.min() + 1.0)*50)
     if(withText):
         TT = np.array(df["text"])
@@ -154,10 +156,10 @@ def plot_scatter_final(df, save2im, withText = True):
 def AggregateTargets(df_targets):
     agg_df_targets = (df_targets.groupby('target').agg({
     'score': ['sum'],
-    'subclass': [ lambda x: list(x)],
-    'color': [lambda x: list(x)] }) ).reset_index()
+    'subcls': [ lambda x: list(x)],
+    'clr': [lambda x: list(x)] }) ).reset_index()
     agg_df_targets.columns = ["_".join(x) for x in agg_df_targets.columns.ravel()]
-    agg_df_targets.columns = ["target", "total_score", "all_classes", "all_colors"]
+    agg_df_targets.columns = ["target", "total_score", "all_clses", "all_clrs"]
     numTargets = agg_df_targets.shape[0]
     numCols = len(hotEncodeColors)
     numTypes = len(hotEncodeTypes)
@@ -169,8 +171,8 @@ def AggregateTargets(df_targets):
         freqColor = np.zeros(numCols, dtype = float)
         freqType = np.zeros(numTypes, dtype=float)
         df = df_targets[df_targets["target"] == target]
-        clrs = np.array(df.color, dtype=int)
-        typs = np.array(df.subclass, dtype=int)
+        clrs = np.array(df.clr, dtype=int)
+        typs = np.array(df.subcls, dtype=int)
         sc = np.array(df.score)
         for i,co in enumerate(clrs):
             freqColor[co - smallestColor] += sc[i]
@@ -178,8 +180,8 @@ def AggregateTargets(df_targets):
         listFreqColor.append(freqColor)
         listFreqType.append(freqType)
 
-    agg_df_targets["freq_color"] = listFreqColor
-    agg_df_targets["freq_class"] = listFreqType
+    agg_df_targets["freq_clr"] = listFreqColor
+    agg_df_targets["freq_cls"] = listFreqType
 
     return(agg_df_targets)
 
@@ -193,9 +195,18 @@ def ComputeDistancesTargets2Clusters(df_T, df_C, wt, bonus_type, bonus_color):
     DCcolors = np.zeros_like(D, dtype=float)
     for tar in range(NT):
         for clus in range(NC):
-            DCcolors[clus,tar]= distance.cosine(df_T["freq_color"][tar] + bonus_color, df_C["freq_color"][clus] + bonus_color)
-            DTypes[clus,tar] = distance.cosine(df_T["freq_class"][tar] + bonus_type, df_C["freq_class"][clus] + bonus_type)
+            DCcolors[clus,tar]= distance.cosine(df_T["freq_clr"][tar] + bonus_color, df_C["freq_clr"][clus] + bonus_color)
+            DTypes[clus,tar] = distance.cosine(df_T["freq_cls"][tar] + bonus_type, df_C["freq_cls"][clus] + bonus_type)
 
     D = DTypes * wt + DCcolors * (1.0 - wt)
     return D
 
+def ReadCSV_or_TXT(all_results_name, data_type):
+    if(data_type is "type1"):
+        df = pd.read_csv(all_results_name)
+
+    else:
+        df = pd.read_csv(all_results_name, sep = ' ')
+        df = df.rename(columns={'lat': 'X', 'lon': 'Y', 'X': 'lat', 'Y': 'lon'})
+
+    return df
